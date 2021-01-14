4314bf67fd0e6803abb46e4539b241ab735a0918
Sohaib Aktar Gazi <aktar19sohaib@gmail.com>
2021-01-14 18:37:16 +0530

    Negetive Number Comes first in Array

---
+++ Array/Negetivefirst.java
@@ -0,0 +1,27 @@
+package array_DSA;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NegetiveFirst {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-->0) {
+			int n= sc.nextInt();
+			
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++) {
+				arr[i] = sc.nextInt();
+			}
+			Arrays.sort(arr);
+			System.out.print("Array after sorted: ");
+			for(int i=0;i<n;i++) {
+				System.out.print(arr[i]+" ");
+			}
+		}
+	
+	}
+}
